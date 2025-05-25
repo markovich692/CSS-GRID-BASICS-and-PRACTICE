89e452ae3521d9eea342cedbf183c65962c7df9f
markovich692 <mark.devguru@outlook.com>
2025-05-24 23:52:54 -0500

    partially solves the kata

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ cannot contain anything but exactly 4 digits or exactly 6 digits.
 
 If the function is passed a valid PIN string, return true, else return false.
 */
+
+function validatePIN(pin) {
+  //return true or false
+
+  const digitCheck = pin
+    .trim()
+    .split("")
+    .every((dig) => isFinite(parseFloat(dig)));
+  const checkSize = pin.length === 4 || pin.length === 6;
+  return digitCheck && checkSize;
+}
+
+const test = validatePIN("123");
+console.log(test);
