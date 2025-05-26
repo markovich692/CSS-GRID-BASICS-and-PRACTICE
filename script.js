2b04b8ac242174a92c5e61343c41c17af8c5ba61
markovich692 <mark.devguru@outlook.com>
2025-05-25 21:54:02 -0500

    adjusts the test1 function adding the split() method to turn the string into an array

--- script.js
+++ script.js
@@ -23,11 +23,12 @@ What if the string is empty? Then the result should be empty object literal, {}.
 
 const test1 = function (s) {
   if (!s.length) return {};
-  return s.reduce((acc, el, i, arr) => {
+  return s.split("").reduce((acc, el, i, arr) => {
     acc[el] = acc[el] ? acc[el] + 1 : 1;
 
     return acc;
   }, {});
 };
 
-const test = test1(["a", "b", "c", "c"]);
+const test = test1("ababbah");
+console.log(test);
