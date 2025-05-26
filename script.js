b302d4b1f45e7ed37dedb13e016be5078184bcce
markovich692 <mark.devguru@outlook.com>
2025-05-25 19:23:02 -0500

    starts a new kata

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
 /*
 Description:
-ATM machines allow 4 or 6 digit PIN codes and PIN codes 
-cannot contain anything but exactly 4 digits or exactly 6 digits.
+Sentence Calculator
+Calculate the total score (sum of the individual 
+character scores) of a sentence given the following
+score rules for each allowed group of characters:
 
-If the function is passed a valid PIN string, return true, else return false.
+Lower case [a-z]: 'a'=1, 'b'=2, 'c'=3, ..., 'z'=26
+Upper case [A-Z]: 'A'=2, 'B'=4, 'C'=6, ..., 'Z'=52
+Digits [0-9] their numeric value: '0'=0, '1'=1, '2'=2, ..., '9'=9
+Other characters: 0 value
 */
-
-function validatePIN(pin) {
-  //return true or false
-
-  const digitCheck = pin
-    .split("")
-    .map((digit) => {
-      return isNaN(parseFloat(digit));
-    })
-    .some((bool) => bool === true)
-    ? false
-    : true;
-
-  const checkSize = pin.trim().length === 4 || pin.trim().length === 6;
-  return digitCheck && checkSize;
-}
-
-const test = validatePIN("12350 ");
-console.log(test);
