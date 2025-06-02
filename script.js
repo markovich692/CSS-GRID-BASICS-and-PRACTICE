7ff1bedb5f71d598e4fbf4b558cf3a74415f96f4
markovich692 <mark.devguru@outlook.com>
2025-06-01 21:54:40 -0500

    starts a new kata

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ Throw       Score
  1 1 1 3 1   1100: 1000 (for three 1s) + 100 (for the other 1)
  2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
  */
-
+/*
 function score(dice) {
   // Fill me in!
   let scoreArr = [];
@@ -91,3 +91,25 @@ function score(dice) {
 }
 
 score([2, 4, 5, 5, 5, 6]);
+*/
+
+/*
+You will be given an array of objects (associative arrays in PHP) 
+representing data about developers who have signed up to attend the
+next coding meetup that you are organising. The list is ordered
+according to who signed up first.
+
+Your task is to return one of the following strings:
+
+< firstName here >, < country here > of the first Python developer who
+ has signed up; or
+There will be no Python developers if no Python developer has signed up.
+For example, given the following input array:
+
+
+var list1 = [
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+];
+*/
