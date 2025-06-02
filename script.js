e251788951f9e0c230ef719c1654b15d53b24734
markovich692 <mark.devguru@outlook.com>
2025-06-01 22:24:07 -0500

    solves the kata

--- script.js
+++ script.js
@@ -107,9 +107,46 @@ There will be no Python developers if no Python developer has signed up.
 For example, given the following input array:
 
 
-var list1 = [
-  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
-  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
-  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
-];
+
 */
+
+function getFirstPython(list) {
+  // Thank you for checking out my kata :)
+
+  const hasPython = list.find((obj) => {
+    return obj.language === "Python";
+  });
+
+  if (!hasPython) return "There will be no Python developers";
+
+  return `${hasPython.firstName}, ${hasPython.country}`;
+}
+
+let list1 = [
+  {
+    firstName: "Mark",
+    lastName: "G.",
+    country: "Scotland",
+    continent: "Europe",
+    age: 22,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Victoria",
+    lastName: "T.",
+    country: "Puerto Rico",
+    continent: "Americas",
+    age: 30,
+    language: "Python",
+  },
+  {
+    firstName: "Emma",
+    lastName: "B.",
+    country: "Norway",
+    continent: "Europe",
+    age: 19,
+    language: "JavaScript",
+  },
+];
+
+console.log(getFirstPython(list1));
