5793320365a62167949774c41a7fbbc12836336c
markovich692 <mark.devguru@outlook.com>
2025-06-08 12:56:21 -0500

    solves the kata

--- script.js
+++ script.js
@@ -254,3 +254,12 @@ The cockroach is one of the fastest insects. Write a function which
 takes its speed in km per hour and returns it in cm per second, rounded
 down to the integer (= floored).
 */
+
+function cockroachSpeed(s) {
+  //Good Luck!
+  if (s < 0) return;
+  const newSpeed = Math.ceil((s * 100000) / (1 * 60 * 60));
+  return newSpeed;
+}
+
+cockroachSpeed(1.05);
