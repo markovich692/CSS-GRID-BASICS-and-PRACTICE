e452486da2f439c06b761c30ed71b5f93253d115
markovich692 <mark.devguru@outlook.com>
2025-06-10 22:28:25 -0500

    starts a new kata

--- script.js
+++ script.js
@@ -302,6 +302,7 @@ If you read "...It Should encode XXX", the "XXX" is the expected result, not
 the input!
 */
 
+/*
 function duplicateEncode(word) {
   let translation = word.toLowerCase();
   let strArr = [...translation];
@@ -324,3 +325,30 @@ function duplicateEncode(word) {
 }
 
 duplicateEncode("Success");
+*/
+
+/*
+Description:
+You received a whatsup message from an unknown number. 
+Could it be from that girl/boy with a foreign accent you 
+met yesterday evening?
+
+Write a simple function to check if the string contains 
+the word hallo in different languages.
+
+These are the languages of the possible people you met the night before:
+
+hello - english
+ciao - italian
+salut - french
+hallo - german
+hola - spanish
+ahoj - czech republic
+czesc - polish
+Notes
+
+you can assume the input is a string.
+to keep this a beginner exercise you don't need to check
+ if the greeting is a subset of word (Hallowen can pass the test)
+function should be case insensitive to pass the tests
+*/
